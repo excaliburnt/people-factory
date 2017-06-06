eaec5091ea5a672f74176c004a3542284362a181
Marshall Brown <marshall.brown12@yahoo.com>
2017-06-06 13:37:00 -0400

    Added input and files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const personForm = document.querySelector('#personForm') // Creates var personForm. It returns form elements 
+
 function handleSubmit(ev) {
   ev.preventDefault() //Stops the default action happening
   const f = ev.target //says which DOM element triggered the event
   const heading = document.querySelector('h1')
-  heading.textContent = f.personName.value //Returns the content of a specefied node
+  heading.textContent = f.personName.value + " is " +  f.personAge.value + " years old." //Returns the content of a specefied node
+  const paragraph = document.querySelector('h2')
+  paragraph.textContent = "Favorite Color is " + f.favColor.value 
 }
+
+  
 
 personForm.addEventListener('submit', handleSubmit) //Adds event to to the button, 'submit'
