84432bcdf2a9c7a78ff87a1770b802d9b3beff66
Marshall Brown <marshall.brown12@yahoo.com>
2017-06-06 09:10:21 -0400

    fix code in html & javascrpt

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const personForm = document.querySelector('#personForm')
-
-// personForm.addEventListener('submit')
-
-function handleSubmit (ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const heading = document.querySelector('hi')
-    heading.textContent = f.personName.value
-    console.log(f.personName.value) //.value gives only the person's name typed
+const personForm = document.querySelector('#personForm') // Creates var personForm. It returns form elements 
+function handleSubmit(ev) {
+  ev.preventDefault() //Stops the default action happening
+  const f = ev.target //says which DOM element triggered the event
+  const heading = document.querySelector('h1')
+  heading.textContent = f.personName.value //Returns the content of a specefied node
 }
 
-personForm.addEventListener('submit', handleSubmit); //Says there's only one argument error
+personForm.addEventListener('submit', handleSubmit) //Adds event to to the button, 'submit'
